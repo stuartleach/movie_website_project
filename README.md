01d2bdc6bd0f3abcd2362f7ac7ead77f92bcc3ff
Stuart Leach <jstuartleach@gmail.com>
2017-06-07 15:16:58 -0400

    added files to begin project and submit to udacity for code review

---
+++ README.md
@@ -0,0 +1,7 @@
+A website creation tool that shows movie-related information.
+
+To use, simply fill entertainment_center.py with instances of the "Movie" class with your favorite movies and various attributes as shown in media.py.
+
+When you are ready to run your program, save all files and run entertainment_center.py.
+
+fresh_tomatoes.py should create an HTML file and open a webbrowser in which to display content.
---
+++ entertainment_center.py
@@ -0,0 +1,26 @@
+import media
+import fresh_tomatoes
+
+# Movies go here
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+avatar = media.Movie("Avatar",
+                    "A marine on an alien planet",
+                    "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                    "https://www.youtube.com/watch?v=uZNHIU3uHT4")
+
+dead_poets_society = media.Movie("Dead Poets Society",
+                                "English teacher John Keating inspires his students to look at poetry with a different perspective of authentic knowledge and feelings.",
+                                "http://t1.gstatic.com/images?q=tbn:ANd9GcSFiizcraYyxtIB2imVhFaWB5eMW1m95_Hp42MVj8Ngxo3Eq386",
+                                "https://www.youtube.com/watch?v=wrBk780aOis")
+
+fargo = media.Movie("Fargo",
+                    "Jerry Lundegaard's inept crime falls apart due to his and his henchmen's bungling and the persistent police work of the quite pregnant Marge Gunderson.",
+                    "https://images-na.ssl-images-amazon.com/images/I/51cf494jBHL.jpg",
+                    "https://www.youtube.com/watch?v=6cJJjr8imTU")
+
+movies = [toy_story, avatar, dead_poets_society, fargo]
+fresh_tomatoes.open_movies_page(movies)
