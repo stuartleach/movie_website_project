1834aa6f92ca822210b53d6433bb177bc00d7a85
Stuart Leach <jstuartleach@gmail.com>
2017-06-12 11:57:46 -0400

    Meet Pep8 requirements

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ To use, simply fill entertainment_center.py with instances of the "Movie" class 
 When you are ready to run your program, save all files and run entertainment_center.py.
 
 fresh_tomatoes.py should create an HTML file and open a webbrowser in which to display content.
+
+# Test
+## Test
+### Test
+#### Test
--- entertainment_center.py
+++ entertainment_center.py
@@ -3,24 +3,44 @@ import fresh_tomatoes
 
 # Movies go here
 
-toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+toy_story = media.Movie("Toy Story", "A story of a boy and \
+                        his toys that come to life",
+                        "http://upload.wikimedia.org/\
+                        wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/\
+                        watch?v=KYz2wyBy3kc")
 
 avatar = media.Movie("Avatar",
-                    "A marine on an alien planet",
-                    "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-                    "https://www.youtube.com/watch?v=uZNHIU3uHT4")
+                     "A marine on an alien planet",
+                     "http://upload.wikimedia.\
+                     org/wikipedia/id/b/b0/Avatar\
+                     -Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch\
+                     ?v=uZNHIU3uHT4")
 
 dead_poets_society = media.Movie("Dead Poets Society",
-                                "English teacher John Keating inspires his students to look at poetry with a different perspective of authentic knowledge and feelings.",
-                                "http://t1.gstatic.com/images?q=tbn:ANd9GcSFiizcraYyxtIB2imVhFaWB5eMW1m95_Hp42MVj8Ngxo3Eq386",
-                                "https://www.youtube.com/watch?v=wrBk780aOis")
+                                 "English teacher John\
+                                  Keating inspires his students \
+                                  to look at poetry with a different \
+                                  perspective of authentic knowledge \
+                                  and feelings.",
+                                 "http://t1.gstatic.com/images?\
+                                 q=tbn:ANd9GcSFiizcraYyxtIB2\
+                                 imVhFaWB5eMW1m95_Hp42MVj8Ngxo3Eq386",
+                                 "https://www.youtube.com/watch?v=wrBk780aOis")
 
 fargo = media.Movie("Fargo",
-                    "Jerry Lundegaard's inept crime falls apart due to his and his henchmen's bungling and the persistent police work of the quite pregnant Marge Gunderson.",
-                    "https://images-na.ssl-images-amazon.com/images/I/51cf494jBHL.jpg",
+                    "Jerry Lundegaard's inept crime falls \
+                    apart due to his and his henchmen's\
+                     bungling and the persistent police \
+                     work of the quite pregnant Marge Gunderson.",
+                    "https://images-na.ssl-images-amazon.com/\
+                    images/I/51cf494jBHL.jpg",
                     "https://www.youtube.com/watch?v=6cJJjr8imTU")
 
+# Create a movies array which contains all movie objects
+
 movies = [toy_story, avatar, dead_poets_society, fargo]
+
+# Use fresh tomatoes to generate an HTML page with movies
 fresh_tomatoes.open_movies_page(movies)
