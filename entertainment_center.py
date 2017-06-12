99c0e63f9c086dd5ee6a6457f946a8ca46db4994
Stuart Leach <jstuartleach@gmail.com>
2017-06-12 12:03:28 -0400

    experiment with line breaks

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,12 +3,11 @@ import fresh_tomatoes
 
 # Movies go here
 
-toy_story = media.Movie("Toy Story", "A story of a boy and \
-                        his toys that come to life",
-                        "http://upload.wikimedia.org/\
-                        wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/\
-                        watch?v=KYz2wyBy3kc")
+toy_story = media.Movie(
+    "Toy Story", "A story of a boy and "
+    "his toys that come to life",
+    "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+    "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
